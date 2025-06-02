e511e3a294c2eb3645e00c7b187fac85750980ce
Gregami <gregami67@gmail.com>
2025-06-02 12:02:45 -0400

    Added new models:
    * prompt (updated)
    * promptgroup

--- nopreserveroot/models.py
+++ nopreserveroot/models.py
@@ -1,9 +1,26 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
 
 
-class prompt(models.Model):
-    name = models.CharField(unique=True)
+class Prompt(models.Model):
+    name = models.CharField(unique=True, null=False)
+    description = models.CharField()
+    score = models.PositiveIntegerField(default=0, null=False)
+
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Category(models.Model):
+    name = models.CharField(unique=True, null=False)
+    description = models.CharField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.name)

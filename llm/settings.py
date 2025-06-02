dec9ccf41d2c8cbcff9cb764afde3a527c80cef8
rkoba0020 <rkoba0020@gmail.com>
2025-06-02 14:48:01 -0400

    Add DB config

--- llm/settings.py
+++ llm/settings.py
@@ -74,10 +74,23 @@ WSGI_APPLICATION = "llm.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "database-1",
+        "USER": "root",
+        "PASSWORD": "c1wkm+719rd!n4",
+        "HOST": "database-1.copqgq8ou71w.us-east-1.rds.amazonaws.com",
+        "PORT": "3306",
     }
 }
 
